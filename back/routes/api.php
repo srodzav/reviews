2555cbb96c56df35d4034cb16106661481b5be3a
Sebastian Zavala <mrjchzz@gmail.com>
2025-09-20 21:28:09 -0600

    feat: backend ready (todo: fix middleware auth routes)

--- back/routes/api.php
+++ back/routes/api.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ReviewController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/movies', [MovieController::class, 'index']);
 Route::get('/movies/search', [MovieController::class, 'search']);
@@ -14,10 +14,5 @@ Route::get('/movies/{id}', [MovieController::class, 'show']);
 Route::post('/movies/from-tmdb', [MovieController::class, 'createFromTMDB']);
 
 Route::get('/reviews', [ReviewController::class, 'index']);
+Route::post('/reviews', [ReviewController::class, 'store']);
 Route::get('/reviews/{id}', [ReviewController::class, 'show']);
-
-Route::middleware('auth')->group(function () {
-    Route::post('/movies', [MovieController::class, 'store']);
-    Route::post('/reviews', [ReviewController::class, 'store']);
-    Route::put('/reviews/{id}', [ReviewController::class, 'update']);
-});
