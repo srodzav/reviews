e952b529f94742e7c01a83a843c9efe861f665cf
Sebastian Zavala <mrjchzz@gmail.com>
2025-09-21 21:54:45 -0600

    fix: backend get reviews without auth

--- back/routes/api.php
+++ back/routes/api.php
@@ -13,8 +13,9 @@ Route::get('/movies/search', [MovieController::class, 'search']);
 Route::get('/movies/{id}', [MovieController::class, 'show']);
 Route::post('/movies/from-tmdb', [MovieController::class, 'createFromTMDB']);
 
+Route::get('/reviews', [ReviewController::class, 'index']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/reviews', [ReviewController::class, 'index']);
     Route::post('/reviews', [ReviewController::class, 'store']);
     Route::get('/reviews/{id}', [ReviewController::class, 'show']);
 });
